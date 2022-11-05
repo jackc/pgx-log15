200b3add954ab721c546cb956fcc2632b19888b4
Jack Christensen <jack@jackchristensen.com>
2022-11-05 10:37:33 -0500

    Update for final v5 with tracelog

--- adapter.go
+++ adapter.go
@@ -5,7 +5,7 @@ package log15
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/tracelog"
 )
 
 // Log15Logger interface defines the subset of
@@ -26,22 +26,22 @@ func NewLogger(l Log15Logger) *Logger {
 	return &Logger{l: l}
 }
 
-func (l *Logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]interface{}) {
+func (l *Logger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]interface{}) {
 	logArgs := make([]interface{}, 0, len(data))
 	for k, v := range data {
 		logArgs = append(logArgs, k, v)
 	}
 
 	switch level {
-	case pgx.LogLevelTrace:
+	case tracelog.LogLevelTrace:
 		l.l.Debug(msg, append(logArgs, "PGX_LOG_LEVEL", level)...)
-	case pgx.LogLevelDebug:
+	case tracelog.LogLevelDebug:
 		l.l.Debug(msg, logArgs...)
-	case pgx.LogLevelInfo:
+	case tracelog.LogLevelInfo:
 		l.l.Info(msg, logArgs...)
-	case pgx.LogLevelWarn:
+	case tracelog.LogLevelWarn:
 		l.l.Warn(msg, logArgs...)
-	case pgx.LogLevelError:
+	case tracelog.LogLevelError:
 		l.l.Error(msg, logArgs...)
 	default:
 		l.l.Error(msg, append(logArgs, "INVALID_PGX_LOG_LEVEL", level)...)
--- go.mod
+++ go.mod
@@ -2,14 +2,11 @@ module github.com/jackc/pgx-log15
 
 go 1.17
 
-require github.com/jackc/pgx/v5 v5.0.0-20211211205502-b2569172d814
+require github.com/jackc/pgx/v5 v5.0.4
 
 require (
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/text v0.3.8 // indirect
 )
